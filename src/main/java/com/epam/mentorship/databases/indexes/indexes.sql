f431c0ea904daafb72810d012f2acb87c5ab46f5
Hurniak Yaroslav <hurnyakyaroslav@gmail.com>
2021-09-26 18:07:10 +0300

    Generate data with and without indexes

--- src/main/java/com/epam/mentorship/databases/indexes/indexes.sql
+++ src/main/java/com/epam/mentorship/databases/indexes/indexes.sql
@@ -1,1 +1,3 @@
 create index surname_b_tree on students using btree(surname);
+create index name_b_tree on students using btree(name);
+create index phone_b_tree on students using btree(phone_number);
