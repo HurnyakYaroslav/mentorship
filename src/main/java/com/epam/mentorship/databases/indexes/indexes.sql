65ba8f9bb0f916c289af5e4938528f919e099082
Hurniak Yaroslav <hurnyakyaroslav@gmail.com>
2021-09-27 23:24:59 +0300

    Add test queries results for b_tree and hash indexes

--- src/main/java/com/epam/mentorship/databases/indexes/indexes.sql
+++ src/main/java/com/epam/mentorship/databases/indexes/indexes.sql
@@ -1,3 +1,8 @@
 create index surname_b_tree on students using btree(surname);
 create index name_b_tree on students using btree(name);
 create index phone_b_tree on students using btree(phone_number);
+
+
+create index surname_hash on students using hash(surname);
+create index name_hash on students using hash(name);
+create index phone_hash on students using hash(phone_number);
--- src/main/java/com/epam/mentorship/databases/queries/findUserByName.sql
+++ src/main/java/com/epam/mentorship/databases/queries/findUserByName.sql
@@ -1,2 +1,2 @@
 EXPLAIN ANALYSE select students.name as name, surname as surname, date_of_birth
-as date_of_birth from students where students.name = 'Yaroslav655';;
+as date_of_birth from students where students.name = 'Yaroslav655';
